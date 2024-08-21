0c0a6689870c5e948f6d27a0b4c586fd4e20f8f8
vobradovich <vobradovich@users.noreply.github.com>
2024-08-21 13:48:45 +0000

    deploy: 073a8548856f89508a350f52342df19a70a167fa

---
+++ pr-4171/gear_common/auxiliary/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["gas_provider","mailbox","task_pool"],"struct":["DoubleBTreeMap"],"trait":["AuxiliaryDoubleStorageWrap","AuxiliaryStorageWrap"],"type":["BlockNumber"]};
